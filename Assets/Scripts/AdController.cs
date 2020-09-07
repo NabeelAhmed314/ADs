59aaab55fe4e586ccbcdbb6fb4a78395ca57cd3e
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 10:57:50 +0500

    first

---
+++ Assets/Scripts/AdController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.Monetization;
+
+public class AdController : MonoBehaviour
+{
+
+    public string store_id ="3304954";
+    public bool testMode = true;
+
+    public string inGameBanner_id = "InGameBanner";
+
+
+    void Start()
+    {
+        Advertisement.Initialize(store_id, testMode);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/AdManagement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdManagement : MonoBehaviour
+{
+    public AdController adController;
+    void Start()
+    {
+        Advertisement.Initialize(adController.store_id,adController.testMode);
+        StartCoroutine(ShowBannerWhenReady());
+    }
+
+    IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(adController.inGameBanner_id))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Banner.Show(adController.inGameBanner_id);
+    }
+}
